115ee94208d998b7094d1b2c4147695bd03c32ad
hossainahmad <hossain.ahmad38@gmail.com>
2026-09-05 17:12:00 +0600

    Created promise to load data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Suspense } from "react";
 import "./App.css";
+import type { CountryType } from "./type";
+import Countries from "./components/Countries";
+
+// Create a promise to load data from API
+const countriesPromise = async (): Promise<CountryType[]> => {
+  const res = await fetch("https://openapi.programming-hero.com/api/all");
+  const data = await res.json();
+  return data.countries;
+};
 
 function App() {
   return (
     <>
-      <h2>Hello from React</h2>
+      <h2>World on the go...</h2>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Countries countriesPromise={countriesPromise()}></Countries>
+      </Suspense>
     </>
   );
 }
