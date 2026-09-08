7ed1e7c7e2d0404d111f1de8861183993941e593
hossainahmad <hossain.ahmad38@gmail.com>
2026-09-08 19:41:58 +0600

    handle visted flags

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { CountryType } from "../../type";
 import "./Country.css";
 
@@ -6,12 +7,25 @@ export interface CountryProps {
 }
 
 export default function Country({ country }: CountryProps) {
+  const [Visited, setVisited] = useState<boolean>(false);
+  const handleVisited = () => {
+    // setVisited(true);
+    if (Visited) {
+      setVisited(false);
+    } else {
+      setVisited(true);
+    }
+  };
+
   return (
-    <div className="country">
+    <div className={`country ${Visited ? "country-visited" : ""}`}>
       <h3>{country.name.common}</h3>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <p>Capital:{country.capital.capital}</p>
       <p>Population: {country.population.population} </p>
+      <button onClick={handleVisited}>
+        {Visited ? "Visited" : "Mark as Visited"}j
+      </button>
     </div>
   );
 }
