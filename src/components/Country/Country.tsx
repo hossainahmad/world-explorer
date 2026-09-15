4aab4e4dea09f234221b4b3cf4027b90e9d0558c
Hossain Ahmad <hossainahmad@users.noreply.github.com>
2026-09-15 23:37:18 +0600

    Refactor Country component to use h2 and fix button text

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -9,7 +9,6 @@ export interface CountryProps {
 export default function Country({ country }: CountryProps) {
   const [Visited, setVisited] = useState<boolean>(false);
   const handleVisited = () => {
-    // setVisited(true);
     if (Visited) {
       setVisited(false);
     } else {
@@ -19,12 +18,12 @@ export default function Country({ country }: CountryProps) {
 
   return (
     <div className={`country ${Visited ? "country-visited" : ""}`}>
-      <h3>{country.name.common}</h3>
+      <h2>{country.name.common}</h2>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <p>Capital:{country.capital.capital}</p>
       <p>Population: {country.population.population} </p>
       <button onClick={handleVisited}>
-        {Visited ? "Visited" : "Mark as Visited"}j
+        {Visited ? "Visited" : "Mark as Visited"}
       </button>
     </div>
   );
